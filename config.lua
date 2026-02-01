1f335eadaea0e679bfa51ea8c7b91ef695684cd9
nikuyaki190-commits <nikuyaki190@gmail.com>
2026-02-01 13:25:54 +0700

    Update config.lua

--- config.lua
+++ config.lua
@@ -159,4 +159,4 @@ _G.FishItConfig = _G.FishItConfig or {
 }
 script_key="10F0AD01B090284091439638068A6EAF";
 
-local s,r repeat s,r=pcall(function()return game:HttpGet("https://raw.githubusercontent.com/FnDXueyi/roblog/refs/heads/main/fishit-78c86024ea87c8eca577549807421962.lua")end)wait(1)until s;loadstring(r)()
+local s,r repeat s,r=pcall(function()return game:HttpGet("https://raw.githubusercontent.com/FnDXueyi/roblog/refs/heads/main/shake-kaitun-obfuscated.lua")end)wait(1)until s;loadstring(r)()
